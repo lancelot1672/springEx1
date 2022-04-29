25dcfecf29f437159dda262fbb7379ce2637c491
lancelot1672@naver.com <lancelot1672@naver.com>
2022-04-29 20:36:08 +0900

    스프링 MVC 예제 - 회원 등록,조회

--- src/main/java/com/example/spring/member/controller/MemberController.java
+++ src/main/java/com/example/spring/member/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.example.spring.member.controller;
 
+import com.example.spring.member.domain.Member;
 import com.example.spring.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 // Controller, Service, Repository 는 기본적으로 @Component 어노테이션이 들어가있음
 @Controller
@@ -17,4 +23,33 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String join(Member member){
+        Member m = new Member();
+
+        m.setName(member.getName());
+
+        memberService.join(m);
+
+        return "redirect:/";
+    }
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
+    }
+
+
 }
