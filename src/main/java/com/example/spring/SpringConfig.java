e7e84a4ae071c04e72c43276ebcfd39eed2d6989
lancelot1672@naver.com <lancelot1672@naver.com>
2022-04-30 17:44:35 +0900

    스프링 예제 - H2 데이터베이스 실습

--- src/main/java/com/example/spring/SpringConfig.java
+++ src/main/java/com/example/spring/SpringConfig.java
@@ -1,20 +1,32 @@
 package com.example.spring;
 
+import com.example.spring.member.repository.JdbcMemberRepository;
 import com.example.spring.member.repository.MemberRepository;
-import com.example.spring.member.repository.MemoryMemberRepository;
 import com.example.spring.member.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
     //자바 코드로 직접 Spring Bean 등록하기
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {    //DataSource 생성자 주입
+        this.dataSource = dataSource;
+    }
+
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
